98eb1aab644cec1934d1a58826eca2c700ea1896
Javier Goizueta <jgoizueta@gmail.com>
2010-06-01 11:37:58 +0200

    Modifications to make the tests runnable with Ruby 1.9.2-preview3

--- test/test_native-float.rb
+++ test/test_native-float.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/test_helper.rb'
+require File.expand_path(File.join(File.dirname(__FILE__),'test_helper.rb'))
 require File.dirname(__FILE__) + '/../lib/float-formats/native.rb'
 require 'yaml'
 
